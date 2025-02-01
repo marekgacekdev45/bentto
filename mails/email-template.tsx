e296e76ce28bdfdc39140f1806c00bea9780491f
marekgacek45 <marekgacek45@gmail.com>
2025-02-01 12:59:46 +0100

    update email templates

--- mails/email-template.tsx
+++ mails/email-template.tsx
@@ -1,28 +1,57 @@
-import { Html, Button,Text,Heading } from "@react-email/components";
-
+import { Button, Heading, Html, Body, Head, Text, Tailwind, Container, Section, Img } from '@react-email/components'
 
 interface EmailTemplateProps {
-	name: string;
-	phone: string;
-	email: string;
-	message: string;
+	name: string
+	phone: string
+	email: string
+	message: string
 }
 
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ? process.env.NEXT_PUBLIC_SITE_URL : ''
+
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ name, phone, email, message }) => (
-	// <div>
-	// 	<h1>Siema, {name}!</h1>
-  //   	<p>Tel: {phone}</p>
-  //   	<p>Email: {email}</p>
-	// 	<p>{message}</p>
-	// </div>
-  <Html lang="pl" dir="ltr">
-     <Heading as="h2">Siema, {name}!</Heading>
-  <Button href={`mailto:${email}`} style={{ color: "#61dafb" }}>
-    Wyślij wiadomość
-  </Button>
-  <Button href={`tel:${phone}`} style={{ color: "#61dafb" }}>
-    Zadzwoń
-  </Button>
-  <Text>{message}</Text>
-</Html>
+	<Html lang='pl' dir='ltr'>
+		<Head />
+
+		<Tailwind>
+			<Body className='bg-white my-auto mx-auto font-sans px-2'>
+				<Container className='border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]'>
+					<Section className='mt-[32px]'>
+						<Img src={`${baseUrl}/mail-logo.png`} width='40' height='37' alt='Bentto' className='my-0 mx-auto' />
+					</Section>
+					<Heading as='h1' className='text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0'>
+						Nowa wiadomość od <strong>{name}</strong>
+					</Heading>
+					<Heading as='h2' className='text-black text-[18px] font-normal p-0 mt-[30px] mx-0'>
+						Oto jej treść:
+					</Heading>
+					<Text className='text-black text-[14px] leading-[24px]'>{message}</Text>
+
+					<Heading as='h2' className='text-black text-[18px] font-normal p-0 mx-0'>
+						Email: <strong>{email}</strong>
+					</Heading>
+					{phone && (
+						<Heading as='h2' className='text-black text-[18px] font-normal p-0 mx-0'>
+							Telefon: <strong>{phone}</strong>
+						</Heading>
+					)}
+
+					<Section className=' text-center mt-[32px] mb-[32px]'>
+						<Button
+							className='bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3 mr-6'
+							href={`mailto:${email}`}>
+							Napisz
+						</Button>
+						{phone && (
+							<Button
+								className='bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3'
+								href={`tel:${phone}`}>
+								Zadzwoń
+							</Button>
+						)}
+					</Section>
+				</Container>
+			</Body>
+		</Tailwind>
+	</Html>
 )
